c8aa843341e646772a0a7044c3b6c9d7cc9928e2
Jake Johnson <jakejjohnson@gmail.com>
2020-01-31 10:49:18 -0600

    implemented search Form using reactstrap

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,10 +1,20 @@
 import React, { useState } from "react";
+import {Form, Input, Button} from 'reactstrap';
 
-export default function SearchForm() {
+export default function SearchForm({search, handleChange, handleSubmit}) {
  
   return (
     <section className="search-form">
      // Add a search form here
+     <Form onSubmit={handleSubmit}>
+        <Input
+          name='search'
+          type='search'
+          search={search}
+          onChange={handleChange}
+        />
+      <Button>submit</Button>
+      </Form>
     </section>
   );
 }
